1db555b2a771ad5cb6702451e873817f846dd36b
xin.liu <xinliu421@gmail.com>
2019-07-03 16:27:10 +0800

    Fix bug

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,4 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     from .api_v1 import api_bp as api_v1_blueprint
-    app.register_blueprint(api_v1_blueprint, url_prefix='request_data/v1')
+    app.register_blueprint(api_v1_blueprint, url_prefix='/request_data/v1')
