f61c54d272784c2c2185991ed5b1775bd77eae29
xin.liu <xinliu421@gmail.com>
2019-07-03 18:13:45 +0800

    Fix error

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,4 @@ def create_app():
     app = Flask(__name__)
     from .api_v1 import api_bp as api_v1_blueprint
     app.register_blueprint(api_v1_blueprint, url_prefix='/request_data/v1')
+    return app
